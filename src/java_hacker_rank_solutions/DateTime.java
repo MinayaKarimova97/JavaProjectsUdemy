0ad2b5f9f0f6c0d309e9ee9797ca76387523042a
Minaye Kerimova <kerimova.minaye@gmail.com>
2025-10-06 17:12:52 +0400

    DateTimeHackerRank
    In this version I updated the code as shown in hacker rank discussion forum. The code's copied from the forum.

--- src/java_hacker_rank_solutions/DateTime.java
+++ src/java_hacker_rank_solutions/DateTime.java
@@ -1,16 +1,54 @@
 package java_hacker_rank_solutions;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
-import java.time.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        int dayOfWeek = calendar.get(calendar.DAY_OF_WEEK);
+        String[] names = new String[] {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        return names[dayOfWeek - 1];
+    }
+
+}
 
 public class DateTime {
-    public static void main(String[] args) {
-        //Calendar rightNow = Calendar.getInstance();
-        //rightNow.set(2025, 9,13);
-        //int day = rightNow.get(Calendar.DAY_OF_WEEK);
-        //System.out.println(day);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        LocalDate today = LocalDate.of(2025, 9, 30);
-        DayOfWeek dayOfWeek = today.getDayOfWeek();
-        System.out.println("Day is: " + dayOfWeek); // e.g., WEDNESDAY
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
+
