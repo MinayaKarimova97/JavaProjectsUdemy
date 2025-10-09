03f9792f8522e02a223b38526bc85d04af453009
Minaye Kerimova <kerimova.minaye@gmail.com>
2025-10-09 16:28:36 +0400

    JavaHackerRankIntroductionLastSolutions
    Bu bolumde Hacker rankde introduction bolumundeki en son gorevler tamamlandi

---
+++ src/java_hacker_rank_solutions/JavaLoops.java
@@ -0,0 +1,27 @@
+package java_hacker_rank_solutions;
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+class JavaLoops{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int s = a; // start with a once
+            for(int j = 0; j < n; j++) {
+                s += (int) (Math.pow(2, j) * b);
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+
+        in.close();
+    }
+}
+
